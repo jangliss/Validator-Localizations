97da3da2e3b1b1c01daee6206f263746c2a76541
jangliss <jon+gh@netdork.net>
2019-07-07 00:48:55 -0500

    - Add PR Locks - Fix URLs -Fix titles/etc - Change from spaces to tabs

--- Validator_TX_localization.user.js
+++ Validator_TX_localization.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                WME Validator Localization for Texas
-// @version             1.0.1
+// @version             1.0.2
 // @author              jangliss
 // @description         This script localizes WME Validator for Texas, USA.
 // @include             /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
@@ -11,103 +11,113 @@
 //
 
 window.WME_Validator_United_States = {
-    ".country": "United States/Texas",
-    ".codeISO": "US",
-    ".author": "jangliss",
-    ".updated": "2018-09-01",
-    ".link": "TODO: ",
+	".country": "United States/Texas",
+	".codeISO": "US",
+	".author": "jangliss",
+	".updated": "2019-07-06",
+	".lng": ["en", "en-US", "en-GB"],
+	".link": "TODO: ",
 
-     //Default US checks
-    "27.enabled": true,
-    "90.enabled": true,
-    "106.enabled": true,
-    "112.enabled": false,
+	 //Default US checks
+	"27.enabled": true,
+	"90.enabled": true,
+	"106.enabled": true,
+	"112.enabled": false,
 
-    "170.enabled": true,
-    "170.params": {
-        "regexp": "/^(?!(to) [^a-z])((S|N|W|E) )?[a-z]/"
-    },
+	"170.enabled": true,
+	"170.params": {
+		"regexp": "/^(?!(to) [^a-z])((S|N|W|E) )?[a-z]/",
+	},
 
-    //  #171 ## Check for improper use of a period (.) that is not on the USA
-    //          recommended abbreviations list (PesachZ)
-    "171.enabled": true,
-    "171.solutionLink": "W:Abbreviations_and_acronyms/USA#Standard_suffix_abbreviations",
-    "171.params": {
-        "regexp": "/((?!(\\bPhila|\\bPenna|.(\\bWash|\\bCmdr|\\bProf|\\bPres)|..(\\bAdm|\\bSte|\\bCpl|\\bMaj|\\bSgt|\\bRe[vc]|\\bR\\.R|\\bGov|\\bGen|\\bHon|\\bCpl)|...(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|...(#| )[NEWSR])).{5}\\.|((?!(hila|enna|(\\bWash|\\bCmdr|\\bProf|\\bPres)|.(\\bAdm|\\bSte|\\bCpl|\\bMaj|\\bSgt|\\bRe[vc]|\\bR\\.R|\\bGov|\\bGen|\\bHon|\\bCpl)|..(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|..(#| )[NEWSR])).{4}|(\\bhila|\\benna))\\.|((?!(ila|nna|(ash|mdr|rof|res)|(\\bAdm|\\bSte|\\bCpl|\\bMaj|\\bSgt|\\bRe[vc]|\\bR\\.R|\\bGov|\\bGen|\\bHon|\\bCpl)|.(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|.(#| )[NEWSR])).{3}|\\b(ila|nna|ash|mdr|rof|res))\\.|((?!(la|na|(sh|dr|of|es)|(dm|te|pl|aj|gt|e[vc]|\\.R|ov|en|on|pl)|(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|(#| )[NEWSR])).{2}|\\b(la|na|sh|dr|of|es|dm|te|pl|aj|gt|e[vc]|\\.R|ov|en|on|pl))\\.|(#|^)[^NEWSR]?\\.)|(((?!\\b(D|O|L)).|#|^)\'(?![sl]\\b)|(#|^)\'s|(?!\\b(In|Na)t).{3}\'l|(#|^).{0,2}\'l)|(Dr|St)\\.(#|$)|,|;|\\\\|((?!\\.( |#|$|R))\\..|(?!\\.( .|#.|$|R\\.))\\..{2}|\\.R(#|$|\\.R))|[Ee]x(p|w)y\\b|\\b[Ee]x[dn]\\b|Tunl\\b|Long Is\\b|Brg\\b/",
-        "problemEN": "The street name has incorrect abbreviation, or character",
-        "solutionEN": "Check upper/lower case, a space before/after the abbreviation and the accordance with the abbreviation table. Remove any comma (,), backslash (\\), or semicolon (;)"
-    },
+	//  #171 ## Check for improper use of a period (.) that is not on the USA
+	//          recommended abbreviations list (PesachZ)
+	"171.enabled": true,
+	"171.problem": "The street name has incorrect abbreviation, or character",
+	"171.solution": "Check upper/lower case, a space before/after the abbreviation and the accordance with the abbreviation table. Remove any comma (,), backslash (\\), or semicolon (;)",
+	"171.solutionLink": "P:USA/Abbreviations_and_acronyms#Standard_suffix_abbreviations",
+	"171.params": {
+		"regexp": "/((?!(\\bPhila|\\bPenna|.(\\bWash|\\bCmdr|\\bProf|\\bPres)|..(\\bAdm|\\bSte|\\bCpl|\\bMaj|\\bSgt|\\bRe[vc]|\\bR\\.R|\\bGov|\\bGen|\\bHon|\\bCpl)|...(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|...(#| )[NEWSR])).{5}\\.|((?!(hila|enna|(\\bWash|\\bCmdr|\\bProf|\\bPres)|.(\\bAdm|\\bSte|\\bCpl|\\bMaj|\\bSgt|\\bRe[vc]|\\bR\\.R|\\bGov|\\bGen|\\bHon|\\bCpl)|..(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|..(#| )[NEWSR])).{4}|(\\bhila|\\benna))\\.|((?!(ila|nna|(ash|mdr|rof|res)|(\\bAdm|\\bSte|\\bCpl|\\bMaj|\\bSgt|\\bRe[vc]|\\bR\\.R|\\bGov|\\bGen|\\bHon|\\bCpl)|.(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|.(#| )[NEWSR])).{3}|\\b(ila|nna|ash|mdr|rof|res))\\.|((?!(la|na|(sh|dr|of|es)|(dm|te|pl|aj|gt|e[vc]|\\.R|ov|en|on|pl)|(\\bSt|\\b[JSD]r|\\bLt|\\bFt)|(#| )[NEWSR])).{2}|\\b(la|na|sh|dr|of|es|dm|te|pl|aj|gt|e[vc]|\\.R|ov|en|on|pl))\\.|(#|^)[^NEWSR]?\\.)|(((?!\\b(D|O|L)).|#|^)\'(?![sl]\\b)|(#|^)\'s|(?!\\b(In|Na)t).{3}\'l|(#|^).{0,2}\'l)|(Dr|St)\\.(#|$)|,|;|\\\\|((?!\\.( |#|$|R))\\..|(?!\\.( .|#.|$|R\\.))\\..{2}|\\.R(#|$|\\.R))|[Ee]x(p|w)y\\b|\\b[Ee]x[dn]\\b|Tunl\\b|Long Is\\b|Brg\\b/",
+	},
 
-    "131.enabled": true,
-    "131.params": {
-      "titleEN": "Wrong banner abbreviation",
-      "problemEN": "Banner abbreviation may be incorrect. Abbreviations ALT, BUS, BYP, CONN, LOOP, and SPUR should be in ALL CAPS",
-      "solutionEN": "Change banner abbreviation to ALT, BUS, BYP, CONN, LOOP, SPUR, or TRUCK",
-      "template": "${street}#${altStreet[#]}",
-      "regexp": "/[0-9]+[A-Z]? ([Aa]lt(ernate)?|[Bb](us(iness)?|yp(ass)?)|[Cc]onn(ector)?|[Ll]oop|[Ss]pur|[Tt]ruck)/"
-    },
-    "131.solutionLink": "W:Road_names/USA#United_States_Numbered_Highways",
+	"131.enabled": true,
+	"131.title": "Wrong banner abbreviation",
+	"131.problem": "Banner abbreviation may be incorrect. Abbreviations ALT, BUS, BYP, CONN, LOOP, and SPUR should be in ALL CAPS",
+	"131.solution": "Change banner abbreviation to ALT, BUS, BYP, CONN, LOOP, SPUR, or TRUCK",
+	"131.solutionLink": "P:USA/Road_names#United_States_Numbered_Highways",
+	"131.params": {
+		"template": "${street}#${altStreet[#]}",
+		"regexp": "/[0-9]+[A-Z]? ([Aa]lt(ernate)?|[Bb](us(iness)?|yp(ass)?)|[Cc]onn(ector)?|[Ll]oop|[Ss]pur|[Tt]ruck)/",
+	},
 
-    "132.enabled": true,
-    "132.title": "Incorrect Segment Name",
-    "132.title.en": "Incorrect Segment Name",
-    "132.params": {
-      "titleEN": "Incorrect Segment Name",
-      "problemEN": "TX uses CR for county road, SH for state highway, US for national highway, and I for interstate names",
-      "solutionEN": "Rename the Street or Alt Street",
-      "template": "${state}:${street}#${altStreet[#]}",
-      "regexp": "/^Texas:.*(?!(SH|CR|US|I)-[0-9]+ ?[A-Za-z]*)([Ii](- | -|-|=| =|= )?|[Uu]\.?[Ss]\.?( [Hh](WY|wy|ighway)| Rte| -|- |-|=| =|= )?|([Tt][Xx]|[Ss]tate|[Cc](ounty|[Oo])) ?([Hh](WY|wy|ighway)|[Rr][Dd]|Rte)|(([Tt][Xx]|([Ss]|[Cc])([Hh]|[Rr]))(- | -|-|=| =|= ))) ?[0-9]+ ?[A-Za-z]*/"
-    },
-    "132.solutionLink": "W:USA/Texas#Roads_in_Texas",
+	"132.enabled": true,
+	"132.title": "Incorrect Segment Name",
+	"132.problem": "TX uses CR for county road, SH for state highway, US for national highway, and I for interstate names",
+	"132.solution": "Rename the Street or Alt Street",
+	"132.solutionLink": "P:USA/Texas#Roads_in_Texas",
+	"132.params": {
+		"template": "${state}:${street}#${altStreet[#]}",
+		"regexp": "/^Texas:.*(?!(SH|CR|US|I)-[0-9]+ ?[A-Za-z]*)([Ii](- | -|-|=| =|= )?|[Uu]\.?[Ss]\.?( [Hh](WY|wy|ighway)| Rte| -|- |-|=| =|= )?|([Tt][Xx]|[Ss]tate|[Cc](ounty|[Oo])) ?([Hh](WY|wy|ighway)|[Rr][Dd]|Rte)|(([Tt][Xx]|([Ss]|[Cc])([Hh]|[Rr]))(- | -|-|=| =|= ))) ?[0-9]+ ?[A-Za-z]*/",
+	},
 
-    "133.solutionLink": "W:USA/Abbreviations_and_acronyms",
+	"133.solutionLink": "P:USA/Abbreviations_and_acronyms",
 
-    "134.solutionLink": "W:USA/Texas#Roads_in_Texas",
+	"134.solutionLink": "P:USA/Texas#Roads_in_Texas",
 
-    "135.enabled": true,
-    "135.params": {
-      "titleEN": "Bad TTS Street name",
-      "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
-      "solutionEN": "Add a period after Jr or St or Dr where required",
-      "template": "${street}#${altStreet[#]}",
-      "regexp": "/^([SNEW] )+(St |Dr )|^St |^Dr |Rev /"
-    },
-    "135.solutionLink": "W:USA/Abbreviations_and_acronyms#Standard_suffix_abbreviations",
+	"135.enabled": true,
+	"135.title": "Bad TTS Street name",
+	"135.problem": "Streets that start with St and Dr result in TTS reading Street or Drive",
+	"135.solution": "Add a period after Jr or St or Dr where required",
+	"135.solutionLink": "P:USA/Abbreviations_and_acronyms",
+	"135.params": {
+		"template": "${street}#${altStreet[#]}",
+		"regexp": "/^([SNEW] )+(St |Dr )|^St |^Dr |Rev /",
+	},
 
-    "136.solutionLink": "W:USA/USA/South_Central#Road_naming",
+	"136.solutionLink": "P:USA/South_Central#Road_naming",
 
-    //Freeway lock
-    "150.enabled": true,
-    "150.params": {
-        "titleEN": "Incorrect lock on Freeway",
-        "n": 5,
-    },
+	// Private Road Lock
+	"137.enabled": true,
+	"137.title": "Incorrect lock on Private Road",
+	"137.problem": "Lock level on Private Road does not match standard",
+	"137.solution": "Correct lock level to standard",
+	"137.solutionLink": "P:USA/USA/South_Central#Road_Lock_Standards",
+	"137.params" : {
+		"template": "${typeRank}:${lock}",
+		"regexp": "/^(17|6):[^2]$/",
+	},
 
-    //Major Highway lock
-    "151.enabled": true,
-    "151.params": {
-        "titleEN": "Incorrect lock on Major Highway",
-        "n": 4,
-    },
+	//Freeway lock
+	"150.enabled": true,
+	"150.title": "Incorrect lock on Freeway",
+	"150.params": {
+		"n": 5,
+	},
 
-    //Minor Highway lock
-    "152.enabled": true,
-    "152.params": {
-        "titleEN": "Incorrect lock on Minor Highway",
-        "n": 3,
-    },
+	//Major Highway lock
+	"151.enabled": true,
+	"151.title": "Incorrect lock on Major Highway",
+	"151.params": {
+		"n": 4,
+	},
 
-    //Ramp lock
-    "153.enabled": true,
-    "153.params": {
-        "titleEN": "Incorrect lock on Ramp",
-        "n": 4,
-    },
+	//Minor Highway lock
+	"152.enabled": true,
+	"152.title": "Incorrect lock on Minor Highway",
+	"152.params": {
+		"n": 3,
+	},
 
-    //Primary Street lock
-    "154.enabled": true,
-    "154.params": {
-        "titleEN": "Incorrect lock on Primary Street",
-        "n": 2,
-    },
+	//Ramp lock
+	"153.enabled": true,
+	"153.title": "Incorrect lock on Ramp",
+	"153.params": {
+		"n": 4,
+	},
+
+	//Primary Street lock
+	"154.enabled": true,
+	"154.title": "Incorrect lock on Primary Street",
+	"154.params": {
+		"n": 2,
+	},
 };
